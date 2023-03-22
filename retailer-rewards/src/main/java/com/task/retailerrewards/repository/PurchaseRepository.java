cdbbdd90f18c81cfef30c83d4eec0c43a5c82618
singhRBGL <70298299+singhRBGL@users.noreply.github.com>
2023-03-22 11:46:09 -0500

    removed junk

--- retailer-rewards/src/main/java/com/task/retailerrewards/repository/PurchaseRepository.java
+++ retailer-rewards/src/main/java/com/task/retailerrewards/repository/PurchaseRepository.java
@@ -15,8 +15,4 @@ public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
 	
 	@Query("SELECT b FROM Purchase b WHERE b.purchaseDate >= :startDate and b.purchaseDate <= :endDate")
 	List<Purchase> findByPurchaseDate(@Param("startDate") Date startDate, @Param("endDate")Date endDate);
-	
-//	@Query("SELECT b FROM Bulk_repository b WHERE b.status = :status and b.campStartDate >=CURRENT_DATE and b.campEndDate <= CURRENT_DATE)
-//			List<Bulk_repository>findAllByStatusAndCampTypeAndCampStartDateGreaterThanEqualAndCampEndDateLessThanEqual(@Param("status") int status);
-
 }
